0452321575e581a6964627a4ddd29e88500828d8
comacrae <comacrae1995@gmail.com>
2025-03-15 15:16:27 -0500

    added __init__ files

--- github_cli_py/tests/github_requester_test.py
+++ github_cli_py/tests/github_requester_test.py
@@ -2,19 +2,19 @@ import pytest
 import typing
 import os
 import dotenv
-import github_activity # https://docs.pytest.org/en/7.1.x/explanation/goodpractices.html#choosing-a-test-layout-import-rules
+from src.github_activity import github_requester # https://docs.pytest.org/en/7.1.x/explanation/goodpractices.html#choosing-a-test-layout-import-rules
 
 
-class GithubRequesterTests:
+class TestGithubRequester:
 
   def test_init_in_valid_state(self) -> None:
-    requester: github_activity.github_requester.GithubRequester = github_activity.github_requester.GithubRequester()
+    requester: github_requester.GithubRequester = github_requester.GithubRequester()
     assert requester.valid_connection() == True
 
   def get_api_key_success(self) -> None:
     dotenv.load_dotenv() 
     api_key: str | None = os.getenv("GITHUB_API_KEY")
-    requester: github_activity.github_requester.GithubRequester = github_activity.github_requester.GithubRequester()
+    requester:github_requester.GithubRequester = github_requester.GithubRequester()
     assert api_key == requester._session.headers["Authorization"]
     
     
