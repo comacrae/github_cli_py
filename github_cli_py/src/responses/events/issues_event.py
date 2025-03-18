b5e2f86a472977c9d0a7bfb84617cdd966f894b7
comacrae <comacrae1995@gmail.com>
2025-03-18 14:04:37 -0500

    moved issues_event_test to correct directory

---
+++ github_cli_py/src/responses/events/issues_event.py
@@ -0,0 +1,13 @@
+import pydantic
+from typing import Mapping
+from github_cli_py.src.responses.events import event_base
+
+class GithubIssuesEventPayload(event_base.GithubEventPayload):
+  action:str
+  issue: Mapping #TODO: create issues object
+  changes: Mapping | None = None # TODO: create changes obj
+  assignee : Mapping | None = None # TODO: create user assigned to issue
+  label: Mapping | None = None #TODO: create label obj
+
+class GithubIssuesEvent(event_base.GithubEvent):
+  payload: GithubIssuesEventPayload
