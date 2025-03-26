f8fa6848736eedd4de1b387e49ae71d1a92b8c8b
Colin MacRae <76566137+comacrae@users.noreply.github.com>
2025-03-26 17:19:59 -0500

    added parser logic  and parsing test(not working yet)

--- github_cli_py/src/responses/events/parser.py
+++ github_cli_py/src/responses/events/parser.py
@@ -1,6 +1,10 @@
-from typing import Mapping
+from typing import Mapping, Optional
 import github_cli_py.src.responses.events.event_types as e
+from github_cli_py.src.responses.events.event_types import event_base
 
+class GithubParseResponseException(Exception):
+  def __init__(self, message:str) -> None:
+    self.message = message
 class GithubEventParser():
   def __init__(self) -> None:
     self._event_map: Mapping = {
@@ -21,4 +25,19 @@ class GithubEventParser():
     }
     return
   
-  def 
+  def parse(self, response_json:str) -> Optional[type[event_base.GithubEvent]]:
+    try:
+      event_type:str = response_json["type"]
+    except KeyError:
+      raise GithubParseResponseException("Event type key not found in response body")
+    
+    try:
+      event_class: type[event_base.GithubEvent] = self._event_map[event_type]
+    except KeyError:
+      raise GithubParseResponseException(f"Event type {event_type} is invalid.")
+    else:
+      return event_class.model_validate_json(response_json)
+
+    
+      
+
--- github_cli_py/tests/events/parser_test.py
+++ github_cli_py/tests/events/parser_test.py
@@ -1,9 +1,35 @@
 import pytest
+from github_cli_py.tests import utils
 from github_cli_py.src.responses.events import parser
+from github_cli_py.src.responses.events.event_types import (
+  event_base, 
+  create_event,
+  delete_event,
+  issue_comment_event,
+  issues_event,
+  pull_request_event)
 
 @pytest.fixture
 def parser_fixture():
   return parser.GithubParser()
 
+
 def test_init_parser_success(parser_fixture: parser.EventParser):
   return
+
+@pytest.mark.parametrize("json_path,event_class",
+                          [("create", create_event.GithubCreateEvent),
+                          ("delete", delete_event.GithubDeleteEvent),
+                          ("issue_comment", issue_comment_event.GithubIssueCommentEvent),
+                          ("issues", issues_event.GithubIssuesEvent),
+                          ("pull_request", pull_request_event.GithubPullRequestEvent)])
+def test_parser_parse_reads_correct_type(
+  event_type:str, 
+  event_class: type[event_base.GithubEvent],
+  parser_fixture: parser.EventParser) -> None:
+  json:str = utils.load_json_resource(f"{event_type}_event_response.json")
+  assert type(parser_fixture.parse(json)) == event_class
+  return
+  
+
+
